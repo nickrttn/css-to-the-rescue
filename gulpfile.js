258c2918fe1fc51a29fd0a65adb0e05d38760c5a
Nick Rutten <hello@nickrttn.me>
2017-02-07 12:59:29 +0100

    Set up autoprefixer
    Added a gulpfile with autoprefixer and browsersync, and a package.json
    to keep track of my dependencies. Also added node_modules to .gitignore.

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var autoprefixer = require('autoprefixer');
+var browserSync = require('browser-sync').create();
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+
+gulp.task('prefix', function () {
+  var plugins = [
+    autoprefixer({ browsers: ['last 4 versions'] }),
+  ];
+  return gulp.src('./*.css')
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest('./build'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('watch', ['prefix'], function() {
+	browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	});
+
+	gulp.watch("./*.css", ['prefix']);
+});
+
+gulp.task('default', ['watch']);
