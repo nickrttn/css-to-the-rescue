39898887274c29a2e90530000475060fdd09ca08
Nick Rutten <hello@nickrttn.me>
2017-02-09 12:14:59 +0100

    updated gulpfile.js to sanitize my workflow a little, added deployment and updated the README

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Components
+# README
+
+The relevant code can be seen in `build/`. An online version is at [csstotherescue.surge.sh](http://csstotherescue.surge.sh).
+
+## Components
 
 Rating example makes use of Emoji from: http://emojione.com
--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,35 @@ var autoprefixer = require('autoprefixer');
 var browserSync = require('browser-sync').create();
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
+var atImport = require('postcss-import');
 
-gulp.task('prefix', function () {
+gulp.task('prefix', function() {
   var plugins = [
-    autoprefixer({ browsers: ['last 4 versions'] }),
+	  atImport,
+    autoprefixer({ browsers: ['last 3 versions'] }),
   ];
-  return gulp.src('./*.css')
+
+  return gulp.src('./src/css/styles.css')
     .pipe(postcss(plugins))
-    .pipe(gulp.dest('./build'))
+    .pipe(gulp.dest('./build/css'))
     .pipe(browserSync.stream());
+});
+
+gulp.task('html', function() {
+	return gulp.src('./src/*.html')
+		.pipe(gulp.dest('./build'))
+		.pipe(browserSync.stream());
 });
 
 gulp.task('watch', ['prefix'], function() {
 	browserSync.init({
 		server: {
-			baseDir: './'
+			baseDir: './build'
 		}
 	});
 
-	gulp.watch("./*.css", ['prefix']);
-	gulp.watch("./components/*.css", ['prefix']);
+	gulp.watch("./src/**/*.css", ['prefix']);
+	gulp.watch('./src/*.html', ['html']);
 });
 
 gulp.task('default', ['watch']);
